38ae99653f417d5a1fbf1875e1499fbd40022a67
Suyash Mishra <mishrasuyash@hotmail.com>
2023-08-20 10:35:04 +0530

    Setup Completed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import Head from 'next/head';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return(<>
+		<Head>
+			<title>Next.js + Tailwind CSS</title>
+			<link rel='icon' href='/favicon.png' />
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      
+		</Head>
+		<Component {...pageProps} />
+	</>)
 }
