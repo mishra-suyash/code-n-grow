e270f02365e71b9ad03af93fdd1f15e9c8156d0e
Suyash Mishra <mishrasuyash@hotmail.com>
2023-08-22 18:01:17 +0530

    Premium Button link fix

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -35,7 +35,7 @@ const Topbar: React.FC<TopbarProps> = ({problemPage}) => {
 				<div className='flex items-center space-x-4 flex-1 justify-end'>
 					<div>
 						<a
-							href='mishrasuyash.vercel.app'
+							href='https://mishrasuyash.vercel.app'
 							target='_blank'
 							rel='noreferrer'
 							className='bg-dark-fill-3 py-1.5 px-3 cursor-pointer rounded text-brand-orange hover:bg-dark-fill-2'
